1c1e18b165865799a83fff6ee9236cd74b365cc8
Vollando <cvb.charles@hotmail.com>
2017-01-25 16:02:22 +0000

    horizontal proximity debugged

--- AudioShooter/Assets/Scripts/raycastScript.cs
+++ AudioShooter/Assets/Scripts/raycastScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class raycastScript : MonoBehaviour {
 
+    CharacterController charCtrl;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+
 	}
 
     // Update is called once per frame
@@ -20,15 +23,26 @@ public class raycastScript : MonoBehaviour {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        CharacterController charContr = GetComponent<CharacterController>();
+        Vector3 p1 = transform.position + charContr.center + Vector3.left * -charContr.height * 0.5f;
+        Vector3 p2 = p1 + Vector3.left * charContr.height;
+        float distanceToObstacle = 0;
+
         if (Physics.Raycast(ray, out hit, 5.0f))
         {
-            Debug.DrawLine(ray.origin, hit.point);
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
             if (hit.collider.gameObject.tag == "Player")
             {
                 Debug.Log("aiming at other player!");
             }
         }
         else Debug.Log("");
+
+        if (Physics.CapsuleCast(p1, p2, charContr.radius, transform.forward, out hit, 5))
+        {
+            distanceToObstacle = hit.distance;
+            Debug.Log(hit.distance);
+        }
 
     }
 }
