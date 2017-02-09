c4871b0bda02334de0a49cbf6c67b2c700181ec5
Vollando <cvb.charles@hotmail.com>
2017-02-09 13:19:38 +0000

    Network Manager added, PlayerSetup scripted

---
+++ AudioShooter/Assets/playerSetupScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerSetupScript : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componenetsToDisable;
+
+    Camera sceneCamera;
+
+    void Start ()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componenetsToDisable.Length; i++)
+            {
+                componenetsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                Camera.main.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void onDisable ()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }   
+    }
+}
