8c1a4ed3ae06ddec9371eac5d4837940c19361d8
Vollando <cvb.charles@hotmail.com>
2017-03-23 19:39:10 +0000

    Players assigned to Network ID

--- AudioShooter/Assets/Scripts/gameManagerScript.cs
+++ AudioShooter/Assets/Scripts/gameManagerScript.cs
@@ -19,9 +19,23 @@ public class gameManagerScript : MonoBehaviour {
         players.Remove(_playerID);
     }
 
+    public static playerManagerScript GetPlayer (string _playerID)
+    {
+        return players[_playerID];
+    }
+
     void OnGUI ()
     {
         GUILayout.BeginArea(new Rect(200, 200, 200, 500));
+        GUILayout.BeginVertical();
+
+        foreach (string _playerID in players.Keys)
+        {
+            GUILayout.Label(_playerID + " - " + players[_playerID].transform.name);
+        }
+
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
     }
 
 }
