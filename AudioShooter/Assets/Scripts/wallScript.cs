05e837f2fe94bc38967dfc3e8e2318a697bbe49f
Vollando <cvb.charles@hotmail.com>
2017-05-08 00:23:16 +0100

    changed audio output

---
+++ AudioShooter/Assets/Scripts/wallScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallScript : MonoBehaviour {
+
+        AudioSource audioSource;
+        public AudioClip wallAudio;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("hit Wall");
+            audioSource.clip = wallAudio;
+            audioSource.Play();
+        }
+    }
+}
